62f731000ce66c88202fa6245e783bcbaa7cac92
schwibbes <schwibbes2@gmail.com>
2017-07-26 20:44:18 +0200

    setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Block />
+       
+    );
+  }
+}
+
+class Block extends Component {
+  render() {
+    return (
+      <div className="Block"></div>
+    );
+  }
+}
+
+rcc
+export default App;
