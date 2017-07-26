f0129a991448b3544fb1a58885ff732def0469a0
schwibbes <schwibbes2@gmail.com>
2017-07-26 22:55:04 +0200

    keytoggle on next

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 import Block from './Block.js';
 
 const KEYS = {
- ESC: 27
+ ESC: 27,
+ // toggles
+ A: 65,
+ S: 83,
+ D: 68,
+ F: 70,
+ // dirs
+ L: 37,
+ R: 39
 };
 
 const LOOKUP = {
@@ -31,7 +39,12 @@ class App extends Component {
         [1, 1, 0, 0],
         [1, 0, 0, 0]
       ],
-      next: [0, 1, 1, 0]
+      next: [0, 1, 1, 0],
+      // 00 -> 0
+      // 01 -> 1
+      // 10 -> 1
+      // 10 -> 0
+      keyToggles: [1, 1, 0, 0]
     }
   }
 
@@ -40,14 +53,79 @@ class App extends Component {
         case KEYS.ESC:
             console.log("esc works")
             break;
+        case KEYS.A:
+            this.setState(prev => {
+              let newToggles = prev.keyToggles.slice()
+              newToggles[0] = 1
+            return {keyToggles: newToggles }
+          })
+            break;
+        case KEYS.S:
+            this.setState(prev => {
+              let newToggles = prev.keyToggles.slice()
+              newToggles[1] = 1
+            return {keyToggles: newToggles }
+          })
+            break;
+        case KEYS.D:
+            this.setState(prev => {
+              let newToggles = prev.keyToggles.slice()
+              newToggles[2] = 1
+            return {keyToggles: newToggles }
+          })
+            break;
+        case KEYS.F:
+            this.setState(prev => {
+              let newToggles = prev.keyToggles.slice()
+              newToggles[3] = 1
+            return {keyToggles: newToggles }
+          })
+            break;
         default: 
-          console.log(event)
+          console.log(event.keyCode)
+            break;
+    }
+  }
+
+  _handleKeyUp (event) {
+    switch( event.keyCode ) {
+        case KEYS.A:
+            this.setState(prev => {
+              let newToggles = prev.keyToggles.slice()
+              newToggles[0] = 0
+            return {keyToggles: newToggles }
+          })
+            break;
+        case KEYS.S:
+            this.setState(prev => {
+              let newToggles = prev.keyToggles.slice()
+              newToggles[1] = 0
+            return {keyToggles: newToggles }
+          })
+            break;
+        case KEYS.D:
+            this.setState(prev => {
+              let newToggles = prev.keyToggles.slice()
+              newToggles[2] = 0
+            return {keyToggles: newToggles }
+          })
+            break;
+        case KEYS.F:
+            this.setState(prev => {
+              let newToggles = prev.keyToggles.slice()
+              newToggles[3] = 0
+            return {keyToggles: newToggles }
+          })
+            break;
+        default: 
+          console.log(event.keyCode)
             break;
     }
   }
 
   componentWillMount() {
     document.addEventListener('keydown', this._handleKeyDown.bind(this))
+    document.addEventListener('keyup', this._handleKeyUp.bind(this))
   }
 
   render() {
@@ -63,7 +141,9 @@ class App extends Component {
     });
 
     let nextBlocks = this.state.next.map( (b,i) => {
-      return <Block key={'n_'+i} filled = {1} txt={LOOKUP[i]}/>
+      let active = b ^ this.state.keyToggles[i]
+      console.log(active)
+      return <Block key={'n_'+i} filled = {active} txt={LOOKUP[i]}/>
     });
 
     return ( 
